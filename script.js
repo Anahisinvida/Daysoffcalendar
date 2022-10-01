882bf723e4912395cb0b4cf60480f6c0211aa87d
“Anahi <parrilla2552@gmail.com>
2022-09-30 19:12:22 -0500

    chore: add style and script

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log("Probando, Probando")
 
+//Inputs HTML
+
 const inputday= document.getElementById("dayInput")
 const inputmonth= document.getElementById("monthInput")
 const inputyear= document.getElementById("yearInput")
@@ -7,7 +9,7 @@ const inputResultado= document.getElementById("resultInput")
 
 console.log("so far i'm just guessing")
 
-//OBJETO 
+//Function
 
 const myDate = () => {
     const weekDate = new Date(inputyear.value, inputmonth.value -1 , inputday.value)
@@ -18,19 +20,22 @@ const myDate = () => {
     switch(getWeekDay){
         case 0:
             weekDay = "Domingo"
+            break;
         case 6:
             weekDay = "Sábado"
              break;
         case 1:
             weekDay = "Lunes"
+            break;
         case 2:
             weekDay = "Martes"
-            
+            break;
         case 3:
             weekDay = "Miércoles"
-         
+            break;
         case 4:
             weekDay = "Jueves"
+            break;
         case 5:
             weekDay = "Viernes"
             break;
@@ -41,11 +46,9 @@ const myDate = () => {
        } else {
         inputResultado.value = `${weekDay} , it's work time`
        }
-
 } 
 
-  
-console.log(`Today is  ${myDate.getDay()} so is beer day`);
+//BOTTON
 const btncalculate = document.getElementById ("btn")
 btncalculate.addEventListener("click", myDate)
 
